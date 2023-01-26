749df077942f587ce32f577177e1418f2df0c873
gaeguriBanchan <rlawowls93@gmail.com>
2023-01-26 13:17:10 +0900

    라우터 기본구조 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import './App.css';
+import { Route } from 'react-router-dom';
+// common
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+// main
+import Content from './components/main/Content';
+import Visual from './components/main/Visual';
+// sub
+import Department from './components/sub/Department';
+import Community from './components/sub/Community';
+import Gallery from './components/sub/Gallery';
+import Youtube from './components/sub/Youtube';
+import Location from './components/sub/Location';
+import Join from './components/sub/Join';
 
 function App() {
-  return(
+  return (
     <>
-      hello
+      <Header />
+      {/*
+        Route: 화면별 Link에 의해 출력될 단위
+        path: 연결할 주소
+        exact: 정확하게 path 가 같을 때만 보여준다.
+      */}
+      <Route exact path='/'>
+        <Visual />
+        <Content />
+      </Route>
+
+      {/*
+        Route: 화면별 Link에 의해 출력될 단위
+        path: 연결할 주소
+      */}
+      {/* Route 1번방식 */}
+      {/* <Route path='/department'>
+        <Department />
+      </Route> */}
+
+      {/* Route 2번방식 : 인라인 방식의 Router 적용 */}
+      <Route path='department' component={Department} />
+      <Route path='community' component={Community} />
+      <Route path='gallery' component={Gallery} />
+      <Route path='youtube' component={Youtube} />
+      <Route path='location' component={Location} />
+      <Route path='join' component={Join} />
+
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App;
