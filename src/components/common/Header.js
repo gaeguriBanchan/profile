3e72077e85da0995ce44359f27183fb1d5b9a603
gaeguriBanchan <rlawowls93@gmail.com>
2023-01-27 12:13:17 +0900

    reset.scss 추가 / header.scss 추가

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -9,29 +9,32 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <h1>
-        <Link to='/'>Logo</Link>
-      </h1>
-      <ul>
-        <li>
-          <Link to='/department'>Department</Link>
-        </li>
-        <li>
-          <Link to='/community'>Community</Link>
-        </li>
-        <li>
-          <Link to='/gallery'>Gallery</Link>
-        </li>
-        <li>
-          <Link to='/youtube'>Youtube</Link>
-        </li>
-        <li>
-          <Link to='/location'>Location</Link>
-        </li>
-        <li>
-          <Link to='/join'>Join</Link>
-        </li>
-      </ul>
+      {/* 레이아웃 div */}
+      <div className='inner'>
+        <h1>
+          <Link to='/'>Logo</Link>
+        </h1>
+        <ul id='gnb'>
+          <li>
+            <Link to='/department'>Department</Link>
+          </li>
+          <li>
+            <Link to='/community'>Community</Link>
+          </li>
+          <li>
+            <Link to='/gallery'>Gallery</Link>
+          </li>
+          <li>
+            <Link to='/youtube'>Youtube</Link>
+          </li>
+          <li>
+            <Link to='/location'>Location</Link>
+          </li>
+          <li>
+            <Link to='/join'>Join</Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
