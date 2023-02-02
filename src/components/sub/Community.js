457c0ff80bcbbf82e14492d494fc6d47a8276147
gaeguriBanchan <rlawowls93@gmail.com>
2023-02-02 10:46:23 +0900

    카카오 지도 1차

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -2,26 +2,12 @@ import React, { useEffect } from 'react'
 import Layout from '../common/Layout'
 
 const Community = () => {
-  // js 코드 자리
-  // 컴포넌트가 마운트, 업데이트(state변경), 언마운트 시
-  // 하고 싶은일을 작성하는 Hook : useEffect
-  // 기본적 형태
-  // useEffect(()=>{
-  //   // 하고싶은일
-  //   // 클린업 함수 : 컴포넌트 제거시 실행
-  //   return ()=>{
-  //   }
-  // },[의존성배열:dependency Array])
-
   useEffect(()=>{
     console.log('mount: 컴포넌트 생성');
-    // axios, html 요소 선택하는 작업...
     return()=>{
       console.log('unmount: 컴포넌트 제거');
     }
   },[])
-  // jsx 코드 자리
-  // : 주의 사항 return()
   return (
     <Layout title={'Community'}>Community</Layout>
   )
